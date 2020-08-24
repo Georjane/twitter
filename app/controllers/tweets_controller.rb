a6466c8a27c4a0d2a1ff8f608b3ae30da6e7b655
Georjane <pzgeorjane@gmail.com>
2020-08-24 13:54:14 +0100

    Migrate Devise for user

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -29,7 +29,7 @@ class TweetsController < ApplicationController
 
     respond_to do |format|
       if @tweet.save
-        format.html { redirect_to @tweet, notice: 'Tweet was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Tweet was successfully created.' }
         format.json { render :show, status: :created, location: @tweet }
       else
         format.html { render :new }
