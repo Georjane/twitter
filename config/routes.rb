42b38803cd0959db700f506ed1d2b2fef915f960
Georjane <pzgeorjane@gmail.com>
2020-08-25 08:52:06 +0100

    Linked tweets to users

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :tweets
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'tweets#index'
+  root 'tweets#index'
 end
  
